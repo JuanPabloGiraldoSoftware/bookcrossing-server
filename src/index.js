58196e498b3783efd3233677b7bdb09d8c91be96
juanpgym <juanpgym@gmail.com>
2021-08-16 19:54:32 -0500

    server/refined-version

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require("body-parser")
 
 //Initialization
 const app = express();
 app.set('port', process.env.PORT || 4000);
 app.use(morgan('dev'));
+app.use(express.json());
 app.listen(app.get('port'), () =>{
     console.log(`Server running on http://localhost:${app.get('port')}`)
 })
 
 //Routes
-app.get('/login', (req, res)=>{
-    res.send("Hlo World")
+app.post('/login', (req, res)=>{
+    console.log(req.body)
+    res.send("Done")
 });
