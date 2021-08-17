fc888d1b1c071f182056e3ff60ae6bcc25e06483
juanpgym <juanpgym@gmail.com>
2021-08-16 20:02:00 -0500

    server/added-missing-headers

--- src/index.js
+++ src/index.js
@@ -7,6 +7,13 @@ const app = express();
 app.set('port', process.env.PORT || 4000);
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+    });
 app.listen(app.get('port'), () =>{
     console.log(`Server running on http://localhost:${app.get('port')}`)
 })
