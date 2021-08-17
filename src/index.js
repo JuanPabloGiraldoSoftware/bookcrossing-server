c0ee9591e435757e38b67713942f646f888e0719
juanpgym <juanpgym@gmail.com>
2021-08-16 20:09:22 -0500

    server/avoiding-cors-request-errors

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const morgan = require('morgan');
-const bodyParser = require("body-parser")
+const cors = require(cors());
 
 //Initialization
 const app = express();
 app.set('port', process.env.PORT || 4000);
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-    });
+app.use(cors());
+app.options('*',cors());
+var allowCrossDomain = function(req,res,next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();  
+}
+app.use(allowCrossDomain);
 app.listen(app.get('port'), () =>{
     console.log(`Server running on http://localhost:${app.get('port')}`)
 })
